10a239132354cc7f007cb280bbccd4576c9370c2
kaanw0145 <kaantr.oguzturk@gmail.com>
2026-05-21 12:29:14 +0300

    final 1.0.0

--- c_ile_notdefteri.c
+++ c_ile_notdefteri.c
@@ -74,6 +74,78 @@ int menu()
 
     return secim;
 }
-void notEkle(FILE *dosya) {}
-void notListele(FILE *dosya) {}
-void notSil(FILE *dosya) {};
+void notEkle(FILE *dosya)
+{
+    struct Not yeniNot, temp;
+
+    int sonID = 0;
+
+    rewind(dosya);
+
+    while (fread(&temp, sizeof(struct Not), 1, dosya))
+    {
+        if (temp.id > sonID)
+        {
+            sonID = temp.id;
+        }
+    }
+
+    yeniNot.id = sonID + 1;
+    yeniNot.aktif = 1;
+
+    printf("Not gir: ");
+    fgets(yeniNot.yazi, 300, stdin);
+
+    yeniNot.yazi[strcspn(yeniNot.yazi, "\n")] = '\0';
+
+    fseek(dosya, 0, SEEK_END);
+
+    fwrite(&yeniNot, sizeof(struct Not), 1, dosya);
+
+    printf("Not eklendi. ID: %d\n", yeniNot.id);
+}
+void notListele(FILE *dosya)
+{
+
+    struct Not not;
+
+    rewind(dosya);
+
+    printf("\n===== NOTLAR =====\n");
+
+    while (fread(&not, sizeof(struct Not), 1, dosya))
+    {
+        if (not.aktif == 1)
+        {
+            printf("%d) %s\n", not.id, not.yazi);
+            printf("-------------------\n");
+        }
+    }
+}
+void notSil(FILE *dosya)
+{
+    struct Not not;
+    int silinecekID;
+
+    printf("Silinecek not ID: ");
+    scanf("%d", &silinecekID);
+
+    rewind(dosya);
+
+    while (fread(&not, sizeof(struct Not), 1, dosya))
+    {
+        if (not.id == silinecekID && not.aktif == 1)
+        {
+            not.aktif = 0;
+
+            fseek(dosya, -sizeof(struct Not), SEEK_CUR);
+
+            fwrite(&not, sizeof(struct Not), 1, dosya);
+            printf("Not silindi.\n");
+
+            return;
+        }
+    }
+
+    printf("Not bulunamadi.\n");
+}
